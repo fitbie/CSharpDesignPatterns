069b07bbe70ae0d015b93584f57b515a83aa9857
Alexander Kuziulenko <fitbiestudio@gmail.com>
2024-03-05 23:22:56 +0700

    refactor: Cleanup

--- Strategy.cs
+++ Strategy.cs
@@ -1,30 +1,42 @@
-// NegativeNumberStrategy strategy = new ArithmeticNegative();
-// Console.WriteLine(strategy.MakeNumberNegative(10));
+namespace GOF.Strategy
+{
 
-// strategy = new BitNegative();
-// Console.WriteLine(strategy.MakeNumberNegative(10));
+    public class GOFStrategy
+    {
+        public void Strategy()
+        {
+            NegativeNumberStrategy strategy = new ArithmeticNegative();
+            Console.WriteLine(strategy.MakeNumberNegative(10));
+
+            strategy = new BitNegative();
+            Console.WriteLine(strategy.MakeNumberNegative(10));
+        }
+
+    }
 
 
-// public abstract class NegativeNumberStrategy
-// {
-//     public abstract int MakeNumberNegative(int number);
-// }
+    public abstract class NegativeNumberStrategy
+    {
+        public abstract int MakeNumberNegative(int number);
+    }
 
 
-// public class ArithmeticNegative : NegativeNumberStrategy
-// {
-//     public override int MakeNumberNegative(int number)
-//     {
-//         if (number > 0) { number *= -1; };
-//         return number;
-//     }
-// }
+    public class ArithmeticNegative : NegativeNumberStrategy
+    {
+        public override int MakeNumberNegative(int number)
+        {
+            if (number > 0) { number *= -1; };
+            return number;
+        }
+    }
 
-// public class BitNegative : NegativeNumberStrategy
-// {
-//     public override int MakeNumberNegative(int number)
-//     {
-//         if (number > 0) { number = (~number) + 1; }
-//         return number;
-//     }
-// }
+    public class BitNegative : NegativeNumberStrategy
+    {
+        public override int MakeNumberNegative(int number)
+        {
+            if (number > 0) { number = (~number) + 1; }
+            return number;
+        }
+    }
+
+}
